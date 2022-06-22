57cc44becd2868a970e4a3e4143f361e975f6d93
agiratech-nikkel <nikkel.r@agiratech.com>
2022-06-22 17:51:59 +0530

    to-de task using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angulartask';
+  newToDo:string 
+  deleted:boolean = false;
+  items:any; 
+  constructor(){
+    this.newToDo=""
+    this.items = []
+  }
+  public addToList(){
+    this.items.push({key: this.newToDo, checked: false})
+    window.localStorage.setItem('task', JSON.stringify(this.newToDo))
+    this.newToDo = ""
+    // alert("Task Add to the list")
+  }
+  public deleteTask(index:any) {
+   this.items.splice(index, 1);
+   // alert("Task is deleted")
+  }
+  changeStatus(index: any) {
+    (this.items[index].checked = !this.items[index].checked)
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule,ReactiveFormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+   MatCardModule,
+   ReactiveFormsModule,
+   FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
