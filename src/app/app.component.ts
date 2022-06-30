b83490bbe3000afe3c3c988f4db9bee558a1cdf8
agiratech-nikkel <102363020+agiratech-nikkel@users.noreply.github.com>
2022-06-30 13:07:59 +0530

    Update app.component.ts
    to-do task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,47 @@
 import { NgClass } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void{
+    this.items = window.localStorage.getItem('task') ? JSON.parse(localStorage.getItem('task')!):[]
+  }
   title = 'angulartask';
   newToDo:string 
   deleted:boolean = false;
-  items:any; 
-  constructor(){
+  items:any;
+  constructor(private router:Router,){
     this.newToDo=""
-    this.items = []
   }
   public addToList(){
+    if(!this.newToDo){
+      alert('please enter the task')
+    }
+    else{
     this.items.push({key: this.newToDo, checked: false})
-    window.localStorage.setItem('task', JSON.stringify(this.newToDo))
+    window.localStorage.setItem('task', JSON.stringify(this.items))
     this.newToDo = ""
-    // alert("Task Add to the list")
   }
+}
   public deleteTask(index:any) {
+    if(confirm("Are you sure to delete "+this.items[index].key +"  task")){
    this.items.splice(index, 1);
-   // alert("Task is deleted")
+   window.localStorage.setItem('task', JSON.stringify(this.items))
+  }
+}
+  changetab(){
+  this.router.navigateByUrl('com')
   }
   changeStatus(index: any) {
     (this.items[index].checked = !this.items[index].checked)
+    window.localStorage.setItem('task', JSON.stringify(this.items))
   }
 }
+
 
